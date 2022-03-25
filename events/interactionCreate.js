d79c0f64a35a55d997df7061707454e1478a80a2
Pre <zzerzv_dev@outlook.com>
2022-03-25 09:41:23 -0400

    Deleted Events Directory
    Deleted Events Directory, For Latest Patch

--- events/interactionCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(` :yay: ${interaction.user.tag} in #${interaction.channel.name} triggered an interaction. :yay: `);
-	},
-};
